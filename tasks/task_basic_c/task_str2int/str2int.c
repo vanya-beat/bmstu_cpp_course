c731517b0a212b10d8935f17466678bc7d515c41
ASLarin <135154483+ASLarin@users.noreply.github.com>
2024-10-04 17:54:45 +0300

    Программа не проходит один  тест

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,10 +1,39 @@
 #include <assert.h>
 #include "str2int.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool is_space(char c) {
+  return (c == ' ');
+}
+bool is_digit(char c) {
+  return (c >= '0' && c <= '9');
+}
 
 
 int str2int(const char *str) {
-    if (*str == '-') {
-        assert(1 == 0);
-    }
-    return 0;
+  if (*str == '-') {
+    assert(1 == 0);
+
+  }
+
+  int result = 0;
+  int sign = 1;
+
+  while (is_space(*str)) {
+    str++;
+  }
+
+  if (*str == '-' || *str == '+') {
+    sign = (*str == '-') ? -1 : 1;
+    str++;
+  }
+
+  while (is_digit(*str)) {
+    int digit = *str - 0x30;
+    result = result * 10 + digit;
+    str++;
+  }
+  return result * sign;
 }
