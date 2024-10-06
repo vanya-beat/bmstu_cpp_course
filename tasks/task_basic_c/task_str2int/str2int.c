3193beaa0fce3042ffb95297e3a420a7845ec50a
vanyabeat <vanyadalle@tutanota.de>
2024-10-06 18:31:23 +0300

    # This is a combination of 6 commits. # This is the 1st commit message:
    Добавлены CI кеши
    
    # This is the commit message #2:
    
    FIX name
    
    # This is the commit message #3:
    
    Update ci-cpp-build.yml
    # This is the commit message #4:
    
    1
    
    # This is the commit message #5:
    
    1
    
    # This is the commit message #6:
    
    Update ci-cpp-build.yml

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,23 +3,5 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    int znak = 1;
-    int result = 0;
-
-    if (*str == '-'){
-        znak = -1;
-        str++;
-    } else if (*str == '+') {
-        str++;
-    }
-
-    int count = 0;
-    while (*str != '\0') {
-        result = result * 10 + (*str - '0');
-        count += 1;
-        str++;
-        assert(znak == -1 && result == -2147483648 || znak == -1 && result >= 0 && count <= 9 || znak == 1 && result >= 0);
-    }
-    assert(count > 0);
-    return result * znak;
+    return 0;
 }
