0c25548adafc2598887f67a43dd5d425be44578f
ifanfomin <ifanfomin@rambler.ru>
2024-10-12 18:17:10 +0300

    настроил cmake debug

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,7 +1,26 @@
+#include <assert.h>
 #include "str2int.h"
 #include "stdio.h"
-#include <assert.h>
 
-int str2int(const char* str) {
-  return 0;
+int str2int(const char *str) {
+    int znak = 1;
+
+    int result = 0;
+
+    if (*str == '-'){
+        znak = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+
+    int count = 0;
+    while (*str != '\0') {
+        result = result * 10 + (*str - '0');
+        count += 1;
+        str++;
+        assert(znak == -1 && result == -2147483648 || znak == -1 && result >= 0 && count <= 9 || znak == 1 && result >= 0);
+    }
+    assert(count > 0);
+    return result * znak;
 }
