82045ad18a1ddf8e1f65550a2ec53577d8597c9f
Evgenii-afk <edahno199@gmail.com>
2024-10-18 00:53:16 +0300

    написал нерабочую прогу int2str

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -3,6 +3,22 @@
 #include "stdio.h"
 
 char* int2str(int number) {
-    char* str = "0";
+    char *str="0";
+    int i = 0;
+
+    for (int num = number; num>0; num/=10){
+        i++;
+    }
+
+    if (number<0){
+        str[0] = '-';
+        i++;
+    }
+
+    
+
+    for ( ; i>0; i--){
+        str[i]=number%10;
+    }
     return str;
 }
