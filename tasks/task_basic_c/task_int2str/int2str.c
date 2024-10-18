2ea2812c51d3b8d7ca79800c77b64f472fb435f2
Misha <125591244+11Hawkstyle00@users.noreply.github.com>
2024-10-18 22:09:29 +0300

    Update int2str.c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -3,6 +3,38 @@
 #include "stdio.h"
 
 char* int2str(int number) {
-    char* str = "0";
+    static char str[12];
+    int i = 0, is_negative = 0;
+    unsigned int num;
+
+    if (number == 0) {
+        str[0] = '0';
+        str[1] = '\0';
+        return str;
+    }
+
+    if (number < 0) {
+        is_negative = 1;
+        num = (number == -2147483648) ? 2147483648 : (unsigned int)(-number);
+    } else {
+        num = number;
+    }
+
+    while (num > 0) {
+        str[i++] = num % 10 + '0';
+        num /= 10;
+    }
+
+    if (is_negative)
+        str[i++] = '-';
+
+    str[i] = '\0';
+
+    for (int j = 0; j < i / 2; j++) {
+        char temp = str[j];
+        str[j] = str[i - 1 - j];
+        str[i - 1 - j] = temp;
+    }
+
     return str;
 }
