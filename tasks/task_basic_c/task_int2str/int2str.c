3412f2087735dde8e7e33c014a74bfee5bc7cbe0
moneyyzzs <sultik2202@gmail.com>
2024-10-18 23:48:27 +0000

    Update int2str.c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,17 +1,29 @@
-#include <stdio.h>  
-#include <stdlib.h>
+#include <stdio.h>
+#include "int2str.h"
 
-int main (void)
-{   
-   char *Str = "123840.sd35663brrt"; 
-   int result=0;               
-   
-   
-   result = atoi(Str);
-   
-   
-   printf ("%d\n",result);
-   
-   
-   return 0; 
+char* int2str(int num) {
+    static char buf[12];
+    int i = 0;
+    long long number = num;
+    int nega = 0;
+
+    if (number == 0) return "0";
+    if (number < 0) {nega = 1; number = -number;}
+
+    while (number > 0) {
+        buf[i++] = (number % 10) + '0';
+        number /= 10;
+    }
+
+    if (nega) buf[i++] = '-';
+
+    buf[i] = '\0';
+
+    for (int j = 0; j < i / 2; j++) {
+        char temp = buf[j];
+        buf[j] = buf[i - j - 1];
+        buf[i - j - 1] = temp;
+    }
+
+    return buf;
 }
