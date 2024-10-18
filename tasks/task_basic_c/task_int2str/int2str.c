109fd19b17679a9cb63952c21248a0acd50f83f1
Evgenii-afk <edahno199@gmail.com>
2024-10-18 22:42:44 +0300

    int2str проходит только четыре теста

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,24 +1,35 @@
 #include <assert.h>
 #include "int2str.h"
 #include "stdio.h"
+#include <malloc.h>
 
 char* int2str(int number) {
-    char *str="0";
     int i = 0;
+    int isNegative = 0;
+    char *str = "0";
+    str = (char*)malloc(20 * sizeof(char));
 
-    for (int num = number; num>0; num/=10){
-        i++;
+    if (number < 0) {
+        isNegative = 1;
+        number = -number;
     }
 
-    if (number<0){
-        str[0] = '-';
-        i++;
+    do {
+        str[i++] = (number % 10) + '0';  
+        number /= 10;                       
+    } while (number > 0);
+
+    if (isNegative==1) {
+        str[i++] = '-';                     
     }
 
+    str[i] = '\0';                         
+
+    for (int j = 0; j < i / 2; j++) {
+        char temp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
+    }
     
-
-    for ( ; i>0; i--){
-        str[i]=number%10;
-    }
     return str;
 }
