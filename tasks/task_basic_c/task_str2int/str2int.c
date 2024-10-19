617e537e068081ec5ee01d4e34993e7b5fa5e666
matthew_balls <68081316+AndrushaCyborg@users.noreply.github.com>
2024-10-19 08:25:36 +0300

    Update str2int.c
    test complete

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,5 +3,26 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    return 0;
+    int sign = 1;
+    if (*str == '-'){
+        sign = -1;
+        str++;
+    }
+    
+    if (*str == '+'){
+        str++;
+    }
+    
+    int integer = 0;
+    int count = 0;
+    
+    while (*str != '\0'){
+        integer = integer * 10 + (*str - *"0");
+        count += 1;
+        str++;
+        assert(sign == -1 && integer == -2147483648 || sign == -1 && integer >= 0 && count <= 9 || sign == 1 && integer >= 0);
+
+    }
+    assert(count > 0);
+    return sign * integer;
 }
