d571205ee1829c92b98067cb4eddbcbe7a25d25e
ifanfomin <ifanfomin@rambler.ru>
2024-10-20 16:24:15 +0300

    Изменил под calloc

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,62 +1,44 @@
-#include <assert.h>
 #include "int2str.h"
-#include "stdio.h"
 
-char* int2str(int num) {
-    long long number = num;
+#include <limits.h>
+#include <stdlib.h>
+
+
+char* int2str(int number) {
     if (number == 0) {
         return "0";
     }
 
-    static char str[15] = "";
-    int znak = 1;
+    if (number == INT_MIN) {
+        return "-2147483648";
+    }
+    
+    char* str = calloc(13, sizeof(char));
+    int number_len = 0;
 
     if (number < 0) {
-        znak = -1;
+        number_len++;
+        str[0] = '-';
         number *= -1;
     }
 
-    int n_len = 0;
-
-    if (number > 0)
-        n_len++;
-    if (number > 10)
-        n_len++;
-    if (number > 100)
-        n_len++;
-    if (number > 1000)
-        n_len++;
-    if (number > 10000)
-        n_len++;
-    if (number > 100000)
-        n_len++;
-    if (number > 1000000)
-        n_len++;
-    if (number > 10000000)
-        n_len++;
-    if (number > 100000000)
-        n_len++;
-    if (number > 1000000000)
-        n_len++;
-    if (number > 10000000000)
-        n_len++;
-
-    if (znak == -1) {
-        n_len++;
-        str[0] = '-';
+    int number_copy = number;
+    while (number_copy > 0) {
+        number_copy /= 10;
+        number_len++;
     }
-    
+
     int digit = 0;
     int count = 0;
-    char ch;
+    char ch = 0;
     while (number > 0) {
         digit = number % 10;
         number = number / 10;
         ch = digit + '0';
-        str[n_len - 1 - count] = ch;
+        str[number_len - 1 - count] = ch;
         count++;
     }
-    str[n_len] = '\0';
+    str[number_len] = '\0';
 
     return str;
 }
