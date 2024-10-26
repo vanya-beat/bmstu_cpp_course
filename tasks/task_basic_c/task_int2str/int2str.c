da1ab28985091d65a9554098c87dcf15b1edcd35
matthew_balls <68081316+MatthewBallz@users.noreply.github.com>
2024-10-26 04:41:17 +0300

    Update int2str.c
    проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,50 @@
 #include <assert.h>
 #include "int2str.h"
 #include "stdio.h"
+#include <stdlib.h>
 
 char* int2str(int number) {
-    char* str = "0";
-    return str;
+    char* str = malloc(12);
+
+    if (str == NULL){
+	return NULL;
+    }
+
+    int i = 0;
+    int sign = number;
+    if (number == 0 | number == -2147483648 | number == -1){
+
+	if (number == 0){
+	    return "0";
+	}
+	if (number == -2147483648){
+	    return "-2147483648";
+	}
+	if (number == -0){
+	    return "0";
+	}
+    }
+    else{
+	if (number < 0){
+	    number = -number;
+	}
+
+	while (number>0){
+	    str[i++] = number % 10 + '0';
+	    number /= 10;
+	}
+
+	if (sign<0){
+	    str[i++] = '-';
+	}
+
+	str[i] = '\0';
+	for (int j = 0, k = i - 1; j < k; j++, k--) {
+	    char temp = str[j];
+	    str[j] = str[k];
+	    str[k] = temp;
+	}
+	
+	return str;
+    }
 }
