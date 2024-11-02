9733fde7b6e07298a7b61317daa2723b5aefc1fa
Fissq <kuznecovdima824@mail.ru>
2024-11-02 12:35:58 +0300

    all work

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,25 @@
-#include <assert.h>
 #include "int2str.h"
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 char* int2str(int number) {
-    char* str = "0";
-    return str;
+    char* str = malloc(12);
+    int i = 10;
+    unsigned int abs_number = (number < 0) ? -number : number;
+    str[11] = '\0';
+
+    if (number == 0) {
+        str[i--] = '0';
+    } else {
+        while (abs_number > 0) {
+            str[i--] = (abs_number % 10) + '0';
+            abs_number /= 10;
+        }
+
+        if (number < 0) {
+            str[i--] = '-';
+        }
+    }
+
+    return &str[i + 1];
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,5 +3,24 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    return 0;
+  int sign = 1;
+  int number = 0;
+  
+  if (*str == '-') {
+    sign = -1;
+    str++;
+  } else if (*str == '+') {
+    str++;
+  }
+  
+  int co = 0;
+  while (*str != '\0') {
+    number = number * 10 + (*str - '0');
+    co += 1;
+    str++;
+    assert(sign == -1 && number == -2147483648 || sign == -1 && number >= 0 && co <= 9 || sign == 1 && number >= 0);
+  }
+  
+  assert(co > 0);
+  return number * sign;
 }
