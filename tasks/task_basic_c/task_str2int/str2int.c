dcf2e90b8c16370caec55bc06aa71166a84ea07e
matthew_balls <annenkov7241@gmail.com>
2024-11-14 11:09:06 +0300

    Fixed style

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -4,25 +4,24 @@
 
 int str2int(const char *str) {
     int sign = 1;
-    if (*str == '-'){
+    if (*str == '-') {
         sign = -1;
         str++;
     }
-    
-    if (*str == '+'){
+
+    if (*str == '+') {
         str++;
     }
-    
-    int integer = 0;
+
+    int intgr = 0;
     int count = 0;
-    
-    while (*str != '\0'){
-        integer = integer * 10 + (*str - *"0");
+
+    while (*str != '\0') {
+        intgr = intgr * 10 + (*str - *"0");
         count += 1;
         str++;
-        assert(sign == -1 && integer == -2147483648 || sign == -1 && integer >= 0 && count <= 9 || sign == 1 && integer >= 0);
-
+        assert(sign == -1 && intgr == -2147483648 || sign == -1 && intgr >= 0 && count <= 9 || sign == 1 && intgr >= 0);
     }
     assert(count > 0);
-    return sign * integer;
+    return sign * intgr;
 }
