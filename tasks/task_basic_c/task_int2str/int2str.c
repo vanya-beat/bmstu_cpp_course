ab0179e996c41575e42377694c219c052b374a21
matthew_balls <annenkov7241@gmail.com>
2024-11-17 16:19:02 +0300

    Styled with 2 spaces indent

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -4,38 +4,38 @@
 #include "stdio.h"
 
 char* int2str(int number) {
-    char* str = malloc(12);
-    int i = 0;
-    int sign = number;
+  char* str = malloc(12);
+  int i = 0;
+  int sign = number;
 
-    if (number == 0) {
-        return "0";
-    }
-    if (number == -2147483648) {
-        return "-2147483648";
-    }
-    if (number == -0) {
-        return "0";
-    }
-    if (number < 0) {
-        number = -number;
-    }
+  if (number == 0) {
+    return "0";
+  }
+  if (number == -2147483648) {
+    return "-2147483648";
+  }
+  if (number == -0) {
+    return "0";
+  }
+  if (number < 0) {
+    number = -number;
+  }
 
-    while (number > 0) {
-        str[i++] = number % 10 + '0';
-        number /= 10;
-    }
+  while (number > 0) {
+    str[i++] = number % 10 + '0';
+    number /= 10;
+  }
 
-    if (sign < 0) {
-        str[i++] = '-';
-    }
+  if (sign < 0) {
+    str[i++] = '-';
+  }
 
-    str[i] = '\0';
-    for (int j = 0, k = i - 1; j < k; j++, k--) {
-        char temp = str[j];
-        str[j] = str[k];
-        str[k] = temp;
-    }
+  str[i] = '\0';
+  for (int j = 0, k = i - 1; j < k; j++, k--) {
+    char temp = str[j];
+    str[j] = str[k];
+    str[k] = temp;
+  }
 
-    return str;
+  return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,25 +3,25 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    int sign = 1;
-    if (*str == '-') {
-        sign = -1;
-        str++;
-    }
+  int sign = 1;
+  if (*str == '-') {
+    sign = -1;
+    str++;
+  }
 
-    if (*str == '+') {
-        str++;
-    }
+  if (*str == '+') {
+    str++;
+  }
 
-    int intgr = 0;
-    int count = 0;
+  int intgr = 0;
+  int count = 0;
 
-    while (*str != '\0') {
-        intgr = intgr * 10 + (*str - *"0");
-        count += 1;
-        str++;
-        assert(sign == -1 && intgr == -2147483648 || sign == -1 && intgr >= 0 && count <= 9 || sign == 1 && intgr >= 0);
-    }
-    assert(count > 0);
-    return sign * intgr;
+  while (*str != '\0') {
+    intgr = intgr * 10 + (*str - *"0");
+    count += 1;
+    str++;
+    assert(sign == -1 && intgr == -2147483648 || sign == -1 && intgr >= 0 && count <= 9 || sign == 1 && intgr >= 0);
+  }
+  assert(count > 0);
+  return sign * intgr;
 }
