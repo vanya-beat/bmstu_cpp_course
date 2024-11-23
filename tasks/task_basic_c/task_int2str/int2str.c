ce23a3215631db88bda1e08261a40651022aa651
matthew_balls <annenkov7241@gmail.com>
2024-11-23 04:26:49 +0300

    cleaning

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,41 +1,30 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "int2str.h"
-#include "stdio.h"
 
 char* int2str(int number) {
   char* str = malloc(12);
   int i = 0;
-  int sign = number;
-
-  if (number == 0) {
-    return "0";
+  unsigned int n = number;
+  if (number < 0) {
+    n = -number;
   }
-  if (number == -2147483648) {
-    return "-2147483648";
-  }
-  if (number == -0) {
-    return "0";
+  while (n > 0) {
+    str[i++] = n % 10 + '0';
+    n /= 10;
   }
   if (number < 0) {
-    number = -number;
-  }
-
-  while (number > 0) {
-    str[i++] = number % 10 + '0';
-    number /= 10;
-  }
-
-  if (sign < 0) {
     str[i++] = '-';
   }
-
+  if (number == 0) {
+    str[0] = '0';
+    i++;
+  }
   str[i] = '\0';
   for (int j = 0, k = i - 1; j < k; j++, k--) {
     char temp = str[j];
     str[j] = str[k];
     str[k] = temp;
   }
-
   return str;
 }
