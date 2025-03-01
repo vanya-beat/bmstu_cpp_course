3c49c4cbdea041f3a0cbabad47bc0c8ee9e10e3c
vanyabeat <vanyabeat@protonmail.com>
2025-03-01 12:38:35 +0300

    Fix: build

--- tasks/bmstu_lets/task_let_1_2/base_algo_let.h
+++ tasks/bmstu_lets/task_let_1_2/base_algo_let.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <vector>
+#include <string>
+#include <cstdint>
+
 /*
 1. Сохранить в отдельный массив только положительные числа из исходного массива.
 2. Отсортировать массив положительных чисел в порядке убывания.
@@ -29,5 +32,5 @@ int sum_positive_numbers(const std::vector<int>& v);
 bool is_divisible_by_10(const std::vector<int>& v);
 void replace_negative_numbers(std::vector<int>& v);
 void double_values(std::vector<int>& v);
-void sort_students_by_age(const std::vector<Student>& v);
-void sort_students_by_name(const std::vector<Student>& v);
+void sort_students_by_age(std::vector<Student>& v);
+void sort_students_by_name(std::vector<Student>& v);
