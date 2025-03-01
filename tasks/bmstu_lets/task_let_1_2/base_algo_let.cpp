0ee92299ecc27ee50614f5ab8e4e1a53159b5d95
matthew_balls <annenkov7241@gmail.com>
2025-03-01 15:28:14 +0300

    letuchka done by andrew

--- tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
+++ tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
@@ -1,25 +1,61 @@
-#include <algorithm>
 #include "base_algo_let.h"
+#include <algorithm>
 
 std::vector<int> positive_numbers(const std::vector<int>& v) {
-  return {};
+  std::vector<int> result;
+  for (int number : v) {
+    if (number > 0) {
+      result.push_back(number);
+    }
+  }
+  return result;
 }
+
 void sort_positive_numbers(std::vector<int>& v) {
+  std::sort(v.begin(), v.end());
 }
+
 int sum_positive_numbers(const std::vector<int>& v) {
-  return 0;
+  int sum = 0;
+  for (int number : v) {
+    if (number > 0) {
+      sum += number;
+    }
+  }
+  return sum;
 }
+
 bool is_divisible_by_10(const std::vector<int>& v) {
+  for (int number : v) {
+    if (number % 10 == 0) {
+      return true;
+    }
+  }
   return false;
 }
+
 void replace_negative_numbers(std::vector<int>& v) {
-  return;
+  for (int& number : v) {
+    if (number < 0) {
+      number = 0;
+    }
+  }
 }
+
 void double_values(std::vector<int>& v) {
-  return;
+  for (int& number : v) {
+    number *= 2;
+  }
 }
+
 void sort_students_by_age(std::vector<Student>& v) {
-  return;
+  std::sort(v.begin(), v.end(), [](const Student& a, const Student& b) {
+    return a.age < b.age;
+  });
 }
+
 void sort_students_by_name(std::vector<Student>& v) {
+  std::sort(v.begin(), v.end(), [](const Student& a, const Student& b) {
+    return a.name < b.name;
+  });
 }
