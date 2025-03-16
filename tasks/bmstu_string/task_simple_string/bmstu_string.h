f11ac894e72424eea42e968c1821c9aaf87062ce
vanyabeat <vanyabeat@protonmail.com>
2025-03-16 18:14:59 +0300

    Pure simple_vector
    Pure simple string
    
    msg

--- tasks/bmstu_string/task_simple_string/bmstu_string.h
+++ tasks/bmstu_string/task_simple_string/bmstu_string.h
@@ -14,32 +14,56 @@ typedef basic_string<char16_t> u16string;
 typedef basic_string<char32_t> u32string;
 
 template <typename T>
+class basic_string;
+
+typedef basic_string<char> string;
+typedef basic_string<wchar_t> wstring;
+// typedef basic_string<char8_t> u8string;
+typedef basic_string<char16_t> u16string;
+typedef basic_string<char32_t> u32string;
+
+template <typename T>
+#ifdef _MSC_VER
 class basic_string
+#else
+class basic_string
+#endif
 {
    public:
 	/// Конструктор по умолчанию
-	basic_string() : ptr_(nullptr), size_(0) {}
+	basic_string() : ptr_(new T[1]{0}), size_(0) {}
 
-	basic_string(size_t size) : ptr_(nullptr), size_(0) {}
+	basic_string(size_t size) : ptr_(new T[size + 1]), size_(size) {}
 
-	basic_string(std::initializer_list<T> il) : ptr_(nullptr), size_(0) {}
+	basic_string(std::initializer_list<T> il)
+		: ptr_(new T[il.size() + 1]), size_(il.size())
+	{
+	}
 
+	/// Конструктор с параметром си-с
 	basic_string(const T* c_str) {}
 
+	/// Конструктор копирования
 	basic_string(const basic_string& other) {}
 
+	/// Перемещающий конструктор
 	basic_string(basic_string&& dying) {}
 
+	/// Деструктор
 	~basic_string() {}
 
-	const T* c_str() const { return nullptr; }
+	/// Геттер на си-строку
+	const T* c_str() const { return ptr_; }
 
 	size_t size() const { return 0; }
 
+	/// Оператор копирующего присваивания
 	basic_string& operator=(basic_string&& other) { return *this; }
 
+	/// Оператор копирующего присваивания си строки
 	basic_string& operator=(const T* c_str) { return *this; }
 
+	/// Оператор копирующего присваивания
 	basic_string& operator=(const basic_string& other) { return *this; }
 
 	friend basic_string<T> operator+(const basic_string<T>& left,
@@ -64,16 +88,16 @@ class basic_string
 
 	basic_string& operator+=(T symbol) { return *this; }
 
-	T& operator[](size_t index) noexcept { return data()[0]; }
+	T& operator[](size_t index) noexcept { return *(ptr_ + index); }
 
-	T& at(size_t index) { return data()[0]; }
+	T& at(size_t index) { throw std::out_of_range("Wrong index"); }
+
+	T* data() { return ptr_; }
 
    private:
 	static size_t strlen_(const T* str) { return 0; }
 
 	void clean_() {}
-
-	T* data() { return nullptr; }
 
 	T* ptr_ = nullptr;
 	size_t size_;
