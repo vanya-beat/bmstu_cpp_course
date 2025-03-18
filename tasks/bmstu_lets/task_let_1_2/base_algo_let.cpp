64d772186c6996cd1746dab5fe27ae88723c4d81
Eugene <super.molchanov@list.ru>
2025-03-18 22:26:03 +0300

    update

--- tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
+++ tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
@@ -3,27 +3,74 @@
 
 std::vector<int> positive_numbers(const std::vector<int>& v)
 {
-	return {};
+	std::vector<int> result;
+	for (int num : v)
+	{
+		if (num > 0)
+		{
+			result.push_back(num);
+		}
+	}
+	return result;
 }
-void sort_positive_numbers(std::vector<int>& v) {}
+
+void sort_positive_numbers(std::vector<int>& v)
+{
+	std::sort(v.begin(), v.end(), std::less<int>());
+}
+
 int sum_positive_numbers(const std::vector<int>& v)
 {
-	return 0;
+	int sum = 0;
+	for (int num : v)
+	{
+		if (num > 0)
+		{
+			sum += num;
+		}
+	}
+	return sum;
 }
+
 bool is_divisible_by_10(const std::vector<int>& v)
 {
+	for (int num : v)
+	{
+		if (num % 10 == 0)
+		{
+			return true;
+		}
+	}
 	return false;
 }
+
 void replace_negative_numbers(std::vector<int>& v)
 {
-	return;
+	for (int& num : v)
+	{
+		if (num < 0)
+		{
+			num = 0;
+		}
+	}
 }
+
 void double_values(std::vector<int>& v)
 {
-	return;
+	for (int& num : v)
+	{
+		num *= 2;
+	}
 }
+
 void sort_students_by_age(std::vector<Student>& v)
 {
-	return;
+	std::sort(v.begin(), v.end(),
+			  [](const Student& a, const Student& b) { return a.age < b.age; });
 }
-void sort_students_by_name(std::vector<Student>& v) {}
+
+void sort_students_by_name(std::vector<Student>& v)
+{
+	std::sort(v.begin(), v.end(), [](const Student& a, const Student& b)
+			  { return a.name < b.name; });
+}
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,62 @@
-#include "str2int.h"
 #include <assert.h>
-#include "stdio.h"
+#include <stdbool.h>
+#include <stdio.h>
+#define int_MAX 2147483647
+#define int_MIN (-2147483647 - 1)
+
+bool is_space(char c)
+{
+	return (c == ' ');
+}
+bool is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
 
 int str2int(const char* str)
 {
-	return 0;
+	int sign = 1;
+
+	if (*str == '\0')
+	{
+		assert(1 == 0);
+	}
+
+	if (*str == '-')
+	{
+		str++;
+		sign = -1;
+		if (*str == '\0')
+		{
+			assert(1 == 0);
+		}
+	}
+	else if (*str == '+')
+	{
+		str++;
+	}
+
+	int result = 0;
+	while (is_space(*str))
+	{
+		str++;
+	}
+
+	while (is_digit(*str))
+	{
+		int digit = *str - '0';
+		if (sign == 1 && (result > (int_MAX - digit) / 10))
+		{
+			assert(1 == 0);
+		}
+		if (sign == -1 && (result > (int_MIN - digit) / 10))
+		{
+			assert(1 == 0);
+		}
+
+		result = result * 10 + digit;
+		str++;
+	}
+
+	return sign * result;
 }
