e40f38250c9b4c7ca46696b3b20aa69e59a8a379
matthew_balls <annenkov7241@gmail.com>
2025-03-22 15:17:45 +0300

    probably fixed int2str.c style

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,30 +1,36 @@
+#include "int2str.h"
 #include <assert.h>
 #include <stdlib.h>
-#include "int2str.h"
 
-char* int2str(int number) {
-  char* str = malloc(12);
-  int i = 0;
-  unsigned int n = number;
-  if (number < 0) {
-    n = -number;
-  }
-  while (n > 0) {
-    str[i++] = n % 10 + '0';
-    n /= 10;
-  }
-  if (number < 0) {
-    str[i++] = '-';
-  }
-  if (number == 0) {
-    str[0] = '0';
-    i++;
-  }
-  str[i] = '\0';
-  for (int j = 0, k = i - 1; j < k; j++, k--) {
-    char temp = str[j];
-    str[j] = str[k];
-    str[k] = temp;
-  }
-  return str;
+char* int2str(int number)
+{
+	char* str = malloc(12);
+	int i = 0;
+	unsigned int n = number;
+	if (number < 0)
+	{
+		n = -number;
+	}
+	while (n > 0)
+	{
+		str[i++] = n % 10 + '0';
+		n /= 10;
+	}
+	if (number < 0)
+	{
+		str[i++] = '-';
+	}
+	if (number == 0)
+	{
+		str[0] = '0';
+		i++;
+	}
+	str[i] = '\0';
+	for (int j = 0, k = i - 1; j < k; j++, k--)
+	{
+		char temp = str[j];
+		str[j] = str[k];
+		str[k] = temp;
+	}
+	return str;
 }
