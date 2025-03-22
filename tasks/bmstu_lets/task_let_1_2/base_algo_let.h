6b7ed0dd39f9dba3b12ec76133bd94b885cbf026
matthew_balls <annenkov7241@gmail.com>
2025-03-22 15:30:45 +0300

    added base_algo_let.h from previous commit

--- tasks/bmstu_lets/task_let_1_2/base_algo_let.h
+++ tasks/bmstu_lets/task_let_1_2/base_algo_let.h
@@ -1,35 +1,76 @@
-#pragma once
-#include <cstdint>
-#include <string>
-#include <vector>
+#include <algorithm>
+#include "base_algo_let.h"
 
-/*
-1. Сохранить в отдельный массив только положительные числа из исходного массива.
-2. Отсортировать массив положительных чисел в порядке убывания.
-3. Найти сумму всех элементов массива положительных чисел.
-4. Проверить, есть ли в исходном массиве хотя бы одно число, кратное 10.
-5. Заменить все отрицательные числа в исходном массиве нулями.
-6. Удвоить все значения оставшихся элементов массива (модифицировать массив
-"наместе").
-7. Отсортировать массив студентов по имени и по возрасту
- */
-
-struct Student
+std::vector<int> positive_numbers(const std::vector<int>& v)
 {
-	Student(const char* name_, int age_) : age(age_), name(name_) {}
-	/// operator ==
-	bool operator==(const Student& other) const
+	std::vector<int> result;
+	for (int number : v)
 	{
-		return other.age == age && other.name == name;
+		if (number > 0)
+		{
+			result.push_back(number);
+		}
 	}
-	uint8_t age;
-	std::string name;
-};
-std::vector<int> positive_numbers(const std::vector<int>& v);
-void sort_positive_numbers(std::vector<int>& v);
-int sum_positive_numbers(const std::vector<int>& v);
-bool is_divisible_by_10(const std::vector<int>& v);
-void replace_negative_numbers(std::vector<int>& v);
-void double_values(std::vector<int>& v);
-void sort_students_by_age(std::vector<Student>& v);
-void sort_students_by_name(std::vector<Student>& v);
+	return result;
+}
+
+void sort_positive_numbers(std::vector<int>& v)
+{
+	std::sort(v.begin(), v.end());
+}
+
+int sum_positive_numbers(const std::vector<int>& v)
+{
+	int sum = 0;
+	for (int number : v)
+	{
+		if (number > 0)
+		{
+			sum += number;
+		}
+	}
+	return sum;
+}
+
+bool is_divisible_by_10(const std::vector<int>& v)
+{
+	for (int number : v)
+	{
+		if (number % 10 == 0)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void replace_negative_numbers(std::vector<int>& v)
+{
+	for (int& number : v)
+	{
+		if (number < 0)
+		{
+			number = 0;
+		}
+	}
+}
+
+void double_values(std::vector<int>& v)
+{
+	for (int& number : v)
+	{
+		number *= 2;
+	}
+}
+
+void sort_students_by_age(std::vector<Student>& v)
+{
+	std::sort(v.begin(), v.end(),
+			  [](const Student& a, const Student& b) { return a.age < b.age; });
+}
+
+void sort_students_by_name(std::vector<Student>& v)
+{
+	std::sort(v.begin(), v.end(), [](const Student& a, const Student& b)
+			  { return a.name < b.name; });
+}
