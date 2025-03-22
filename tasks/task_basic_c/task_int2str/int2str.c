f1635b2464cf06af632d66126016e16be8b3139a
Dmitriy <80101092+Vokaksab@users.noreply.github.com>
2025-03-22 15:58:38 +0300

    Done

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,9 +1,25 @@
 #include "int2str.h"
-#include <assert.h>
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-char* int2str(int number)
-{
-	char* str = "0";
-	return str;
+char *int2str(int num) {
+    char *buffer = calloc(12, sizeof(char));
+    unsigned int positive_num = (num < 0) ? -num : num;
+    buffer[11] = '\0';
+    int index = 10;
+    if (num == 0) {
+        buffer[index] = '0';
+        index--;
+    } else {
+        while (positive_num > 0) {
+            buffer[index] = (positive_num % 10) + '0';
+            positive_num /= 10;
+            index--;
+        }
+        if (num < 0) {
+            buffer[index] = '-';
+            index--;
+        }
+    }
+    return &buffer[index + 1];
 }
