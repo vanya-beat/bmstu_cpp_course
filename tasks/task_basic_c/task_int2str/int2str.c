0243272eabe3e62d815313b367e27edfb36dd3a8
Greenliess <v-morozov00@yandex.ru>
2025-03-29 00:40:57 +0300

    update int2str

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,58 +1,62 @@
-#include <stdio.h>
 #include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-char* int2str(int number) {
-    
-    if (number == 0) {
-        char *zero_str = malloc(2 * sizeof(char)); 
-        if (zero_str == NULL) return NULL; 
-        zero_str[0] = '0';
-        zero_str[1] = '\0';
-        return zero_str;
-    }
+char* int2str(int number)
+{
+	if (number == 0)
+	{
+		char* zero_str = malloc(2 * sizeof(char));
+		if (zero_str == NULL)
+			return NULL;
+		zero_str[0] = '0';
+		zero_str[1] = '\0';
+		return zero_str;
+	}
 
-    // Обработка INT_MIN
-    if (number == INT_MIN) {
-        return strdup("-2147483648"); 
-    }
+	// Обработка INT_MIN
+	if (number == INT_MIN)
+	{
+		return strdup("-2147483648");
+	}
 
-    
-    int is_negative = number < 0;
-    if (is_negative) {
-        number = -number; 
-    }
+	int is_negative = number < 0;
+	if (is_negative)
+	{
+		number = -number;
+	}
 
-    
-    int num_len = 0;
-    int temp = number;
-    do {
-        num_len++;
-        temp /= 10;
-    } while (temp > 0);
-    
-    // Учитываем знак
-    if (is_negative) {
-        num_len++;
-    }
+	int num_len = 0;
+	int temp = number;
+	do
+	{
+		num_len++;
+		temp /= 10;
+	} while (temp > 0);
 
-    
-    char *str = malloc((num_len + 1) * sizeof(char)); 
-    if (str == NULL) return NULL; 
+	// Учитываем знак
+	if (is_negative)
+	{
+		num_len++;
+	}
 
-    
-    str[num_len] = '\0'; 
-    int idx = num_len - 1;
+	char* str = malloc((num_len + 1) * sizeof(char));
+	if (str == NULL)
+		return NULL;
 
-    while (number > 0) {
-        str[idx--] = (number % 10) + '0'; 
-        number /= 10;
-    }
+	str[num_len] = '\0';
+	int idx = num_len - 1;
 
-    
-    if (is_negative) {
-        str[idx] = '-';
-    }
+	while (number > 0)
+	{
+		str[idx--] = (number % 10) + '0';
+		number /= 10;
+	}
 
-    return str; 
+	if (is_negative)
+	{
+		str[idx] = '-';
+	}
+
+	return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,36 @@
+#include <stdio.h> 
+#include "stdbool.h" 
+#include <assert.h> 
 #include "str2int.h"
-#include <assert.h>
-#include "stdio.h"
-
-int str2int(const char* str)
-{
-	return 0;
+ 
+bool space(char c) { 
+    return (c == ' '); 
+} 
+bool digit(char c) {     
+    return (c >= '0' & c <= '9'); 
+} 
+int str2int(const char *str) { 
+    int poly4aetca = 0; 
+    int znak = 1;
+    bool base = 1;
+    while (space(*str)) { 
+        str++;     
+    } 
+    if (*str == '-' || *str == '+') { 
+        znak = (*str == '-') ? -1 : 1;         
+        str++; 
+    } 
+    while (digit(*str)) {       
+        int digit = *str - 0x30;  
+        if (((znak == -1) && (poly4aetca > (-2147483647 - 1 - digit) / 10)) || ((znak == 1) && (poly4aetca > (2147483647 - digit) / 10))) { 
+            assert(0==1); 
+        } 
+        poly4aetca = poly4aetca * 10 + digit;  
+        base = 0;      
+        str++; 
+    } 
+    if (base) {
+        assert(0==1);
+    }
+    return poly4aetca * znak; 
 }
