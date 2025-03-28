f39274b5d8430bf466effd1410754b95b860bc72
greenliess <you@example.com>
2025-03-29 00:16:13 +0300

    str2int

---
+++ build/tasks/bmstu_lets/CMakeFiles/bmstu_lets.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bmstu_lets.dir/task_let_1_2/base_algo_let.cpp.o"
+  "CMakeFiles/bmstu_lets.dir/task_let_1_2/base_algo_let.cpp.o.d"
+  "CMakeFiles/bmstu_lets.dir/task_let_1_2/base_algo_let_tests.cpp.o"
+  "CMakeFiles/bmstu_lets.dir/task_let_1_2/base_algo_let_tests.cpp.o.d"
+  "bmstu_lets"
+  "bmstu_lets.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bmstu_lets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tasks/bmstu_lets/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/greenliess/projects/bmstu_cpp_course_greenliess/bmstu_cpp_course_greenliess/tasks/bmstu_lets
+# Build directory: /home/greenliess/projects/bmstu_cpp_course_greenliess/bmstu_cpp_course_greenliess/build/tasks/bmstu_lets
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
