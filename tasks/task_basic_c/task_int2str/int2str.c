5294c210325e261ed4d91c40cb5547cb99996669
ifanfomin <ifanfomin@rambler.ru>
2025-03-28 08:18:21 +0300

    no style errors

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,39 +1,44 @@
 #include "int2str.h"
 #include <stdlib.h>
 
+char* int2str(int num)
+{
+	char* str = (char*)calloc(13, sizeof(char));
+	int number_len = 0;
 
-char* int2str(int num) {
-    char* str = (char*)calloc(13, sizeof(char));
-    int number_len = 0;
+	unsigned int number = 0;
+	if (num < 0)
+	{
+		number_len++;
+		str[0] = '-';
+		number = num * -1;
+	}
+	else
+	{
+		number = num;
+	}
 
-    unsigned int number = 0;
-    if (num < 0) {
-        number_len++;
-        str[0] = '-';
-        number = num * -1;
-    } else {
-        number = num;
-    }
+	unsigned int number_copy = number;
+	int count = 0;
+	while ((number_copy == 0 && count < 1) || number_copy > 0)
+	{
+		number_copy /= 10;
+		number_len++;
+		count++;
+	}
 
-    unsigned int number_copy = number;
-    int count = 0;
-    while ((number_copy == 0 && count < 1) || number_copy > 0) {
-        number_copy /= 10;
-        number_len++;
-        count++;
-    }
+	int digit = 0;
+	count = 0;
+	char ch = 0;
+	while ((number == 0 && count < 1) || number > 0)
+	{
+		digit = number % 10;
+		number = number / 10;
+		ch = digit + '0';
+		str[number_len - 1 - count] = ch;
+		count++;
+	}
+	str[number_len] = '\0';
 
-    int digit = 0;
-    count = 0;
-    char ch = 0;
-    while ((number == 0 && count < 1) || number > 0) {
-        digit = number % 10;
-        number = number / 10;
-        ch = digit + '0';
-        str[number_len - 1 - count] = ch;
-        count++;
-    }
-    str[number_len] = '\0';
-
-    return str;
+	return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,26 +1,33 @@
-#include <assert.h>
 #include "str2int.h"
+#include <assert.h>
 #include "stdio.h"
 
-int str2int(const char *str) {
-    int znak = 1;
+int str2int(const char* str)
+{
+	int znak = 1;
 
-    int result = 0;
+	int result = 0;
 
-    if (*str == '-'){
-        znak = -1;
-        str++;
-    } else if (*str == '+') {
-        str++;
-    }
+	if (*str == '-')
+	{
+		znak = -1;
+		str++;
+	}
+	else if (*str == '+')
+	{
+		str++;
+	}
 
-    int count = 0;
-    while (*str != '\0') {
-        result = result * 10 + (*str - '0');
-        count += 1;
-        str++;
-        assert(znak == -1 && result == -2147483648 || znak == -1 && result >= 0 && count <= 9 || znak == 1 && result >= 0);
-    }
-    assert(count > 0);
-    return result * znak;
+	int count = 0;
+	while (*str != '\0')
+	{
+		result = result * 10 + (*str - '0');
+		count += 1;
+		str++;
+		assert(znak == -1 && result == -2147483648 ||
+			   znak == -1 && result >= 0 && count <= 9 ||
+			   znak == 1 && result >= 0);
+	}
+	assert(count > 0);
+	return result * znak;
 }
