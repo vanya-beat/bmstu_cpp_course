3344222be4f9a4760f4521060ae2bde038a2a3f6
omn1tron <axmat.akbaev@icloud.com>
2025-03-28 21:17:48 +0300

    Восстановление репозитория: task_c_fix

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,34 +1,41 @@
-#include "int2str.h"
 #include <stdlib.h>
+#include "int2str.h"
 
-char* int2str(const int number) {
-  int size = 0;
-  unsigned int temp_number = number;
-  unsigned int ext_number;
+char* int2str(const int number)
+{
+	int size = 0;
+	unsigned int temp_number = number;
+	unsigned int ext_number;
 
-  if (number < 0) {
-    ext_number = -number;
-    temp_number = -temp_number;
-    size++;
-  } else {
-    ext_number = number;
-  }
+	if (number < 0)
+	{
+		ext_number = -number;
+		temp_number = -temp_number;
+		size++;
+	}
+	else
+	{
+		ext_number = number;
+	}
 
-  do {
-    size++;
-    temp_number /= 10;
-  } while (temp_number > 0);
+	do
+	{
+		size++;
+		temp_number /= 10;
+	} while (temp_number > 0);
 
-  char* buf = malloc(size + 1);
-  buf[size] = '\0';
+	char* buf = malloc(size + 1);
+	buf[size] = '\0';
 
-  for (int i = size - 1; i >= 0; i--) {
-    buf[i] = (ext_number % 10) + '0';
-    ext_number /= 10;
-  }
+	for (int i = size - 1; i >= 0; i--)
+	{
+		buf[i] = (ext_number % 10) + '0';
+		ext_number /= 10;
+	}
 
-  if (number < 0) {
-    buf[0] = '-';
-  }
-  return buf;
+	if (number < 0)
+	{
+		buf[0] = '-';
+	}
+	return buf;
 }
