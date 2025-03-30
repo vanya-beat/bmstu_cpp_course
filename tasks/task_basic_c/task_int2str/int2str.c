495a5f1fcd9e403207584fc3e75a309c200c0197
Greenliess <v-morozov00@yandex.ru>
2025-03-31 01:38:13 +0300

    int2str

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,65 +1,34 @@
-#include <limits.h>
-#include <stdio.h>
+#include "int2str.h"
+#include <assert.h>
+#include "stdio.h"
 #include <stdlib.h>
-#include <string.h>
 
 char* int2str(int number)
 {
-	if (number == 0)
-	{
-		char* zero_str = malloc(2 * sizeof(char));
-		if (zero_str == NULL)
-			return NULL;
-		zero_str[0] = '0';
-		zero_str[1] = '\0';
-		return zero_str;
-	}
-
-	if (number == INT_MIN)
-	{
-		char* min_str = malloc(12 * sizeof(char));
-		if (min_str == NULL)
-			return NULL;
-		strcpy(min_str, "-2147483648");
-		return min_str;
-	}
-
-	int is_negative = number < 0;
-	if (is_negative)
-	{
-		number = -number;
-	}
-
-	int num_len = 0;
-	int temp = number;
-	while (temp > 0)
-	{
-		num_len++;
-		temp /= 10;
-	}
-
-	if (is_negative)
-	{
-		num_len++;
-	}
-
-	char* str = malloc((num_len + 1) * sizeof(char));
-	if (str == NULL)
-		return NULL;
-
-	str[num_len] = '\0';
-	int idx = num_len - 1;
-
-	do
-	{
-		str[idx--] = (number % 10) + '0';
-		number /= 10;
-	} while (number > 0);
-
-	if (is_negative)
-	{
-		str[0] = '-';
-	}
-
-	return str;
+    int is_negative = number < 0;
+    unsigned int n = is_negative ? -((unsigned int)number) : number;
+    
+    int length = 0;
+    unsigned int temp = n;
+    do {
+        length++;
+        temp /= 10;
+    } while (temp > 0);
+    
+    if (is_negative) length++;
+    
+    char* str = (char*)malloc(length + 1);
+    if (!str) return NULL;
+    
+    str[length] = '\0';
+    
+    for (int i = length - 1; i >= (is_negative ? 1 : 0); i--) {
+        str[i] = '0' + (n % 10);
+        n /= 10;
+    }
+    
+    if (is_negative) str[0] = '-';
+    if (length == 1 && str[0] == '0' && number == 0) return str;
+    
+    return str;
 }
