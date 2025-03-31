88bc92a113a489b414e2c108f780549940edb6c0
Dmitriy <80101092+Vokaksab@users.noreply.github.com>
2025-03-31 19:24:34 +0300

    Update int2str.c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,27 +1,44 @@
 #include "int2str.h"
-
-#include <stdio.h>
 #include <stdlib.h>
 
-char* int2str(int num) {
-    char* buffer = calloc(12, sizeof(char));
-    unsigned int positive_num = (num < 0) ? -num : num;
-    buffer[11] = '\0';
-    int index = 10;
+char* int2str(int num)
+{
+	char* str = (char*)calloc(13, sizeof(char));
+	int number_len = 0;
 
-    if (num == 0) {
-        buffer[index] = '0';
-        index--;
-    } else {
-        while (positive_num > 0) {
-            buffer[index] = (positive_num % 10) + '0';
-            positive_num /= 10;
-            index--;
-        }
-        if (num < 0) {
-            buffer[index] = '-';
-            index--;
-        }
-    }
-    return &buffer[index + 1];
+	unsigned int number = 0;
+	if (num < 0)
+	{
+		number_len++;
+		str[0] = '-';
+		number = num * -1;
+	}
+	else
+	{
+		number = num;
+	}
+
+	unsigned int number_copy = number;
+	int count = 0;
+	while ((number_copy == 0 && count < 1) || number_copy > 0)
+	{
+		number_copy /= 10;
+		number_len++;
+		count++;
+	}
+
+	int digit = 0;
+	count = 0;
+	char ch = 0;
+	while ((number == 0 && count < 1) || number > 0)
+	{
+		digit = number % 10;
+		number = number / 10;
+		ch = digit + '0';
+		str[number_len - 1 - count] = ch;
+		count++;
+	}
+	str[number_len] = '\0';
+
+	return str;
 }
