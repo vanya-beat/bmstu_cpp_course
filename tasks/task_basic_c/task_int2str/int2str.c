12802d5361835f656956666034cc88874b5e8e37
Yspex9 <chpokreacher@gmail.com>
2025-03-31 11:10:25 +0300

    Правки int2str

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -4,41 +4,40 @@
 char* int2str(int num)
 {
 	char* str = (char*)calloc(13, sizeof(char));
+
 	int number_len = 0;
 
-	unsigned int number = 0;
+	unsigned int number = abs(num);
+
 	if (num < 0)
 	{
+		str[number_len++] = '-';
+	}
+	else if ( num == 0)
+	{
+		str[number_len++] = '0';
+	}
+
+	unsigned int temp = number;
+	if (number == 0)
+	{
 		number_len++;
-		str[0] = '-';
-		number = num * -1;
 	}
 	else
 	{
-		number = num;
+		while (temp > 0)
+		{
+			number_len++;
+			temp /= 10;
+		}
 	}
 
-	unsigned int number_copy = number;
-	int count = 0;
-	while ((number_copy == 0 && count < 1) || number_copy > 0)
-	{
-		number_copy /= 10;
-		number_len++;
-		count++;
-	}
-
-	int digit = 0;
-	count = 0;
-	char ch = 0;
-	while ((number == 0 && count < 1) || number > 0)
-	{
-		digit = number % 10;
-		number = number / 10;
-		ch = digit + '0';
-		str[number_len - 1 - count] = ch;
-		count++;
-	}
 	str[number_len] = '\0';
+	while (number > 0)
+	{
+		str[--number_len] = (number % 10) + '0';
+		number /= 10;
+	}
 
 	return str;
 }
