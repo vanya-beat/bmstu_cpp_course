3f955928fbeb120116f4b34208454217887ef56f
Greenliess <v-morozov00@yandex.ru>
2025-03-31 19:04:08 +0300

    new str2int

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include "str2int.h"
 
@@ -11,39 +12,47 @@ int str2int(const char* str)
 	}
 
 	int sign = 1;
-	long long result = 0;
 	int i = 0;
+	int number = 0;
+	bool negative_overflow = false;
+	bool positive_overflow = false;
 
 	if (str[0] == '-')
 	{
 		sign = -1;
-		i++;
+		i = 1;
 	}
 
 	for (; str[i] != '\0'; i++)
 	{
-		if (str[i] >= '0' && str[i] <= '9')
+		if (str[i] < '0' || str[i] > '9')
 		{
-			result = result * 10 + (str[i] - '0');
+			return 0;
+		}
 
-			if ((sign == 1 && result > INT_MAX) ||
-				(sign == -1 && result > (long long)INT_MAX + 1))
+		int digit = str[i] - '0';
+
+		if (sign == 1)
+		{
+			if (number > INT_MAX / 10 ||
+				(number == INT_MAX / 10 && digit > INT_MAX % 10))
 			{
 				return 0;
 			}
 		}
 		else
 		{
-			return 0;
+			if (number > INT_MAX / 10 ||
+				(number == INT_MAX / 10 && digit > (INT_MAX % 10 + 1)))
+			{
+				return 0;
+			}
 		}
+
+		number = number * 10 + digit;
 	}
 
-	result *= sign;
+	number = number * sign;
 
-	if (result < INT_MIN || result > INT_MAX)
-	{
-		return 0;
-	}
-
-	return (int)result;
+	return number;
 }
