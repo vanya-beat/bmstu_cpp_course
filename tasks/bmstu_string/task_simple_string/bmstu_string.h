7d42b7a648574481a55cf538a331d11a356c7b83
Greenliess <v-morozov00@yandex.ru>
2025-04-01 00:40:44 +0300

    string

--- tasks/bmstu_string/task_simple_string/bmstu_string.h
+++ tasks/bmstu_string/task_simple_string/bmstu_string.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <algorithm>
 #include <exception>
 #include <iostream>
 
@@ -15,12 +14,17 @@ typedef basic_string<char16_t> u16string;
 typedef basic_string<char32_t> u32string;
 
 template <typename T>
+#ifdef _MSC_VER
 class basic_string
+#else
+class basic_string
+#endif
 {
    public:
+	/// Конструктор по умолчанию
 	basic_string() : ptr_(new T[1]{0}), size_(0) {}
 
-	explicit basic_string(size_t size) : ptr_(new T[size + 1]), size_(size)
+	basic_string(size_t size) : ptr_(new T[size + 1]), size_(size)
 	{
 		std::fill(ptr_, ptr_ + size_, static_cast<T>(' '));
 		ptr_[size_] = 0;
@@ -33,6 +37,7 @@ class basic_string
 		ptr_[size_] = 0;
 	}
 
+	/// Конструктор с параметром си-с
 	basic_string(const T* c_str)
 	{
 		if (c_str)
@@ -48,31 +53,54 @@ class basic_string
 		}
 	}
 
+	/// Конструктор копирования
 	basic_string(const basic_string& other)
 		: ptr_(new T[other.size_ + 1]), size_(other.size_)
 	{
 		std::copy(other.ptr_, other.ptr_ + other.size_ + 1, ptr_);
 	}
 
-	basic_string(basic_string&& other) noexcept
-		: ptr_(other.ptr_), size_(other.size_)
+	/// Перемещающий конструктор
+	basic_string(basic_string&& dying) noexcept
+		: ptr_(dying.ptr_), size_(dying.size_)
 	{
-		other.ptr_ = new T[1]{0};
-		other.size_ = 0;
+		dying.ptr_ = new T[1]{0};
+		dying.size_ = 0;
 	}
 
+	/// Деструктор
 	~basic_string() { delete[] ptr_; }
 
+	/// Геттер на си-строку
+	const T* c_str() const
+	{
+		return ptr_ ? ptr_ : reinterpret_cast<const T*>("");
+	}
+
+	size_t size() const { return size_; }
+
+	/// Оператор копирующего присваивания
 	basic_string& operator=(const basic_string& other)
 	{
 		if (this != &other)
 		{
 			basic_string tmp(other);
-			swap(tmp);
+			std::swap(ptr_, tmp.ptr_);
+			std::swap(size_, tmp.size_);
 		}
 		return *this;
 	}
 
+	/// Оператор копирующего присваивания си строки
+	basic_string& operator=(const T* c_str)
+	{
+		basic_string tmp(c_str);
+		std::swap(ptr_, tmp.ptr_);
+		std::swap(size_, tmp.size_);
+		return *this;
+	}
+
+	/// Оператор присваивания перемещением
 	basic_string& operator=(basic_string&& other) noexcept
 	{
 		if (this != &other)
@@ -86,62 +114,10 @@ class basic_string
 		return *this;
 	}
 
-	basic_string& operator=(const T* c_str)
+	friend basic_string<T> operator+(const basic_string<T>& left,
+									 const basic_string<T>& right)
 	{
-		basic_string tmp(c_str);
-		swap(tmp);
-		return *this;
-	}
-
-	const T* c_str() const
-	{
-		return ptr_ ? ptr_ : reinterpret_cast<const T*>("");
-	}
-	size_t size() const { return size_; }
-
-	T& operator[](size_t index) noexcept { return ptr_[index]; }
-	const T& operator[](size_t index) const noexcept { return ptr_[index]; }
-
-	T& at(size_t index)
-	{
-		if (index >= size_)
-			throw std::out_of_range("Index out of range");
-		return ptr_[index];
-	}
-
-	const T& at(size_t index) const
-	{
-		if (index >= size_)
-			throw std::out_of_range("Index out of range");
-		return ptr_[index];
-	}
-
-	T* data() { return ptr_; }
-	const T* data() const { return ptr_; }
-
-	basic_string& operator+=(const basic_string& other)
-	{
-		*this = *this + other;
-		return *this;
-	}
-
-	basic_string& operator+=(T symbol)
-	{
-		T* new_ptr = new T[size_ + 2];
-		if (ptr_)
-			std::copy(ptr_, ptr_ + size_, new_ptr);
-		new_ptr[size_] = symbol;
-		new_ptr[size_ + 1] = 0;
-		delete[] ptr_;
-		ptr_ = new_ptr;
-		++size_;
-		return *this;
-	}
-
-	friend basic_string operator+(const basic_string& left,
-								  const basic_string& right)
-	{
-		basic_string result;
+		basic_string<T> result;
 		result.size_ = left.size_ + right.size_;
 		result.ptr_ = new T[result.size_ + 1];
 		std::copy(left.ptr_, left.ptr_ + left.size_, result.ptr_);
@@ -170,6 +146,45 @@ class basic_string
 		return is;
 	}
 
+	basic_string& operator+=(const basic_string& other)
+	{
+		*this = *this + other;
+		return *this;
+	}
+
+	basic_string& operator+=(T symbol)
+	{
+		T* new_ptr = new T[size_ + 2];
+		if (ptr_)
+			std::copy(ptr_, ptr_ + size_, new_ptr);
+		new_ptr[size_] = symbol;
+		new_ptr[size_ + 1] = 0;
+		delete[] ptr_;
+		ptr_ = new_ptr;
+		++size_;
+		return *this;
+	}
+
+	T& operator[](size_t index) noexcept { return ptr_[index]; }
+	const T& operator[](size_t index) const noexcept { return ptr_[index]; }
+
+	T& at(size_t index)
+	{
+		if (index >= size_)
+			throw std::out_of_range("Wrong index");
+		return ptr_[index];
+	}
+
+	const T& at(size_t index) const
+	{
+		if (index >= size_)
+			throw std::out_of_range("Wrong index");
+		return ptr_[index];
+	}
+
+	T* data() { return ptr_; }
+	const T* data() const { return ptr_; }
+
    private:
 	static size_t strlen_(const T* str)
 	{
@@ -186,13 +201,7 @@ class basic_string
 		size_ = 0;
 	}
 
-	void swap(basic_string& other) noexcept
-	{
-		std::swap(ptr_, other.ptr_);
-		std::swap(size_, other.size_);
-	}
-
-	T* ptr_;
+	T* ptr_ = nullptr;
 	size_t size_;
 };
 }  // namespace bmstu
