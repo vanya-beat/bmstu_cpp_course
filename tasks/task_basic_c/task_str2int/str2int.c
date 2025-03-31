c4cc8dd7b9c6e6c0ce54dec30f9ec1ae5c1df4ae
Dmitriy <80101092+Vokaksab@users.noreply.github.com>
2025-03-31 19:27:57 +0300

    Update str2int.c

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -6,36 +6,35 @@
 #define int_MIN (-2147483647 - 1)
 
 bool is_digit(char x) {
-  return (x >= '0' && x <= '9');
+    return (x >= '0' && x <= '9');
 }
 
 int str2int(const char *str) {
-  int sign = 1;
-  int result = 0;
-
-  if (*str == '\0') {
-    assert(1 == 0);
-  }
-
-  if (*str == '-') {
-    sign = -1;
-    str++;
+    int sign = 1;
+    int result = 0;
     if (*str == '\0') {
-      assert(1 == 0);
+        assert(1 == 0);
     }
+
+    if (*str == '-') {
+        sign = -1;
+        str++;
+        if (*str == '\0') {
+            assert(1 == 0);
+        }
   } else if (*str == '+') {
-    str++;
+      str++;
   }
 
-  while (is_digit(*str)) {
-    int digit = *str - '0';
-    if (sign == 1 && (result > (int_MAX - digit) / 10)) {
-      assert(1 == 0);
-    } else if (sign == -1 && (result > (int_MIN - digit) / 10)) {
-      assert(1 == 0);
+    while (is_digit(*str)) {
+        int digit = *str - '0';
+        if (sign == 1 && (result > (int_MAX - digit) / 10)) {
+            assert(1 == 0);
+        } else if (sign == -1 && (result > (int_MIN - digit) / 10)) {
+            assert(1 == 0);
+        }
+        result = result * 10 + digit;
+        str++;
     }
-    result = result * 10 + digit;
-    str++;
-  }
-  return sign * result;
+    return sign * result;
 }
