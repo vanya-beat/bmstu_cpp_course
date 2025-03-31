36bd22af6088fa08674db3b3bd11d75e3466ab57
Dmitriy <80101092+Vokaksab@users.noreply.github.com>
2025-03-31 19:11:30 +0300

    Update int2str.c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,25 +1,27 @@
 #include "int2str.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-char *int2str(int num) {
-    char *buffer = calloc(12, sizeof(char));
-    unsigned int positive_num = (num < 0) ? -num : num;
-    buffer[11] = '\0';
-    int index = 10;
-    if (num == 0) {
-        buffer[index] = '0';
-        index--;
-    } else {
-        while (positive_num > 0) {
-            buffer[index] = (positive_num % 10) + '0';
-            positive_num /= 10;
-            index--;
-        }
-        if (num < 0) {
-            buffer[index] = '-';
-            index--;
-        }
+#include <stdio.h>
+#include <stdlib.h>
+
+char* int2str(int num) {
+  char* buffer = calloc(12, sizeof(char));
+  unsigned int positive_num = (num < 0) ? -num : num;
+  buffer[11] = '\0';
+  int index = 10;
+
+  if (num == 0) {
+    buffer[index] = '0';
+    index--;
+  } else {
+    while (positive_num > 0) {
+      buffer[index] = (positive_num % 10) + '0';
+      positive_num /= 10;
+      index--;
     }
-    return &buffer[index + 1];
+    if (num < 0) {
+      buffer[index] = '-';
+      index--;
+    }
+  }
+  return &buffer[index + 1];
 }
