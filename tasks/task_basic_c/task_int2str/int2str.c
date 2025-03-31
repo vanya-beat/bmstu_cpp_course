3817e1e333ce53211349453e1fb4a19d4b0b692e
Васильев Дмитрий <dpro08@bk.ru>
2025-03-31 23:51:05 +0300

    str2int done

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,9 +1,63 @@
-#include "int2str.h"
 #include <assert.h>
-#include "stdio.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-char* int2str(int number)
-{
-	char* str = "0";
-	return str;
+char* int2str(int number) {
+    // Специальный случай для минимального значения int
+    if (number == INT_MIN) {
+        char* result = malloc(12); // "-2147483648" + '\0'
+        if (result == NULL) return NULL;
+        sprintf(result, "%d", INT_MIN);
+        return result;
+    }
+
+    // Определяем знак числа
+    bool is_negative = number < 0;
+    if (is_negative) {
+        number = -number;
+    }
+
+    // Вычисляем длину числа
+    int length = 0;
+    int temp = number;
+    do {
+        temp /= 10;
+        length++;
+    } while (temp != 0);
+
+    // Выделяем память (знак + цифры + нуль-терминатор)
+    char* str = malloc(length + is_negative + 1);
+    if (str == NULL) return NULL;
+
+    // Заполняем строку с конца
+    int pos = length + is_negative;
+    str[pos--] = '\0';
+    
+    do {
+        str[pos--] = (number % 10) + '0';
+        number /= 10;
+    } while (number != 0);
+
+    // Добавляем знак минус если нужно
+    if (is_negative) {
+        str[0] = '-';
+    }
+
+    return str;
+}
+
+// Тесты
+void test_int2str() {
+    assert(strcmp(int2str(0), "0") == 0);
+    assert(strcmp(int2str(123), "123") == 0);
+    assert(strcmp(int2str(-456), "-456") == 0);
+    assert(strcmp(int2str(INT_MAX), "2147483647") == 0);
+    assert(strcmp(int2str(INT_MIN), "-2147483648") == 0);
+}
+
+int main() {
+    test_int2str();
+    return 0;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,59 @@
-#include "str2int.h"
 #include <assert.h>
-#include "stdio.h"
+#include <limits.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-int str2int(const char* str)
+int str2int(const char* str) 
 {
-	return 0;
+    // Проверка на NULL указатель
+    assert(str != '0');
+    
+    // Пропускаем ведущие пробелы (если нужно)
+    while (isspace(*str)) {
+        str++;
+    }
+
+    // Определяем знак числа
+    int sign = 1;
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    } 
+    else if (*str == '+') {
+        str++;
+    }
+
+    // Проверяем, что после знака есть цифры
+    assert(isdigit(*str));
+
+    int result = 0;
+    bool overflow = false;
+    const int max_safe = INT_MAX / 10;
+    const int last_digit_max = INT_MAX % 10;
+
+    while (isdigit(*str)) {
+        int digit = *str - '0';
+        
+        // Проверка на переполнение
+        if (result > max_safe || 
+            (result == max_safe && digit > last_digit_max)) {
+            overflow = true;
+            break;
+        }
+
+        result = result * 10 + digit;
+        str++;
+    }
+
+    // Проверка на корректное завершение строки
+    assert(*str == '\0');
+    assert(!overflow);
+
+    // Применяем знак с проверкой на INT_MIN
+    if (sign == -1) {
+        assert(result <= INT_MAX); // INT_MIN обрабатывается особо
+        return (result == INT_MAX) ? INT_MIN : -result;
+    }
+
+    return result;
 }
