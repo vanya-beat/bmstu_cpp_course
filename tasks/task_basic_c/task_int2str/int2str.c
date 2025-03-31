c828192f0cbefef2d9a79c6c79a210f17ce1128d
Greenliess <v-morozov00@yandex.ru>
2025-03-31 22:23:51 +0300

    fix

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,42 +1,36 @@
-#include <stdlib.h>
 #include "int2str.h"
+#include <stdlib.h>
 
-static int calculate_number_len(unsigned int number)
-{
-	int length = 0;
-	do
-	{
-		length++;
-		number /= 10;
-	} while (number > 0);
-	return length;
+static int calculate_number_len(unsigned int number) {
+  int length = 0;
+  do {
+    length++;
+    number /= 10;
+  } while (number > 0);
+  return length;
 }
 
-char* int2str(int num)
-{
-	int is_negative = num < 0;
-	unsigned int number = is_negative ? -num : num;
-	int number_len = calculate_number_len(number);
-	int str_len = number_len + (is_negative ? 1 : 0) + 1;
-	char* str = (char*)calloc(str_len, sizeof(char));
+char* int2str(int num) {
+  int is_negative = num < 0;
+  unsigned int number = is_negative ? -num : num;
+  int number_len = calculate_number_len(number);
+  int str_len = number_len + (is_negative ? 1 : 0) + 1;
+  char* str = (char*)calloc(str_len, sizeof(char));
 
-	if (str == NULL)
-	{
-		return NULL;
-	}
+  if (str == NULL) {
+    return NULL;
+  }
 
-	if (is_negative)
-	{
-		str[0] = '-';
-	}
+  if (is_negative) {
+    str[0] = '-';
+  }
 
-	for (int i = number_len - 1; i >= 0; i--)
-	{
-		str[is_negative ? i + 1 : i] = (number % 10) + '0';
-		number /= 10;
-	}
+  for (int i = number_len - 1; i >= 0; i--) {
+    str[is_negative ? i + 1 : i] = (number % 10) + '0';
+    number /= 10;
+  }
 
-	str[str_len - 1] = '\0';
+  str[str_len - 1] = '\0';
 
-	return str;
+  return str;
 }
