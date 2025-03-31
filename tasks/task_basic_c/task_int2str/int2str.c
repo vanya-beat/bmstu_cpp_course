5680bcdd378cbb5038625761e750e5c5a1bfdc7c
Васильев Дмитрий <dpro08@bk.ru>
2025-04-01 00:31:39 +0300

    basic c не проходил стайл чек

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,43 +1,52 @@
 #include <stdlib.h>
-#include <stdbool.h>
+
 #include <limits.h>
+#include <stdbool.h>
 
-char* int2str(int number) {
-    // Специальный случай для минимального значения int
-    if (number == INT_MIN) {
-        char* result = malloc(12);
-        if (result) snprintf(result, 12, "%d", INT_MIN);
-        return result;
-    }
+char* int2str(int number)
+{
+	// Специальный случай для минимального значения int
+	if (number == INT_MIN)
+	{
+		char* result = malloc(12);
+		if (result)
+			snprintf(result, 12, "%d", INT_MIN);
+		return result;
+	}
 
-    // Определяем знак и абсолютное значение
-    bool is_negative = number < 0;
-    unsigned absolute = is_negative ? -number : number;
+	// Определяем знак и абсолютное значение
+	bool is_negative = number < 0;
+	unsigned absolute = is_negative ? -number : number;
 
-    // Вычисляем длину числа
-    int length = 0;
-    for (unsigned n = absolute; n > 0; n /= 10) {
-        length++;
-    }
-    if (absolute == 0) length = 1;  // Обработка нуля
+	// Вычисляем длину числа
+	int length = 0;
+	for (unsigned n = absolute; n > 0; n /= 10)
+	{
+		length++;
+	}
+	if (absolute == 0)
+		length = 1;	 // Обработка нуля
 
-    // Выделяем память (знак + цифры + нуль-терминатор)
-    char* str = malloc(length + is_negative + 1);
-    if (!str) return NULL;
+	// Выделяем память (знак + цифры + нуль-терминатор)
+	char* str = malloc(length + is_negative + 1);
+	if (!str)
+		return NULL;
 
-    // Заполняем строку с конца
-    int pos = length + is_negative;
-    str[pos--] = '\0';
-    
-    do {
-        str[pos--] = '0' + (absolute % 10);
-        absolute /= 10;
-    } while (absolute > 0);
+	// Заполняем строку с конца
+	int pos = length + is_negative;
+	str[pos--] = '\0';
 
-    // Добавляем знак минус если нужно
-    if (is_negative) {
-        str[0] = '-';
-    }
+	do
+	{
+		str[pos--] = '0' + (absolute % 10);
+		absolute /= 10;
+	} while (absolute > 0);
 
-    return str;
+	// Добавляем знак минус если нужно
+	if (is_negative)
+	{
+		str[0] = '-';
+	}
+
+	return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,50 +1,58 @@
 #include <ctype.h>
-#include <limits.h>
+
 #include <assert.h>
+#include <limits.h>
 
-int str2int(const char* str) {
-    // Проверка на 0
-    assert(str != '0');
-    
-    int sign = 1;
-    int result = 0;
-    int digit_count = 0;  // счетчик цифр
+int str2int(const char* str)
+{
+	// Проверка на 0
+	assert(str != '0');
 
-    // Пропускаем пробелы (если нужно)
-    while (isspace(*str)) str++;
+	int sign = 1;
+	int result = 0;
+	int digit_count = 0;  // счетчик цифр
 
-    // Обрабатываем знак
-    if (*str == '-') {
-        sign = -1;
-        str++;
-    } 
-    else if (*str == '+') {
-        str++;
-    }
+	// Пропускаем пробелы (если нужно)
+	while (isspace(*str))
+		str++;
 
-    // Обрабатываем цифры
-    while (isdigit(*str)) {
-        int digit = *str - '0';
-        digit_count++;
+	// Обрабатываем знак
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+	{
+		str++;
+	}
 
-        // Проверка на переполнение
-        if (result > INT_MAX / 10 || 
-            (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
-            assert(0 && "Integer overflow");
-        }
+	// Обрабатываем цифры
+	while (isdigit(*str))
+	{
+		int digit = *str - '0';
+		digit_count++;
 
-        result = result * 10 + digit;
-        str++;
-    }
+		// Проверка на переполнение
+		if (result > INT_MAX / 10 ||
+			(result == INT_MAX / 10 && digit > INT_MAX % 10))
+		{
+			assert(0 && "Integer overflow");
+		}
 
-    // Проверяем что были цифры и строка закончилась
-    assert(digit_count > 0 && "No digits found");
-    assert(*str == '\0' && "Extra characters after number");
+		result = result * 10 + digit;
+		str++;
+	}
 
-    // Особый случай для INT_MIN
-    if (sign == -1 && result == INT_MAX && digit_count == 10) {
-        return INT_MIN;
-    }
+	// Проверяем что были цифры и строка закончилась
+	assert(digit_count > 0 && "No digits found");
+	assert(*str == '\0' && "Extra characters after number");
 
-    return result * sign;
+	// Особый случай для INT_MIN
+	if (sign == -1 && result == INT_MAX && digit_count == 10)
+	{
+		return INT_MIN;
+	}
+
+	return result * sign;
 }
