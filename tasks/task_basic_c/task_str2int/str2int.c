bce0956490001d6ad98e98ccdf0fb7eefb937ea2
Dmitriy <80101092+Vokaksab@users.noreply.github.com>
2025-03-31 19:28:59 +0300

    Update str2int.c

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,40 +1,33 @@
 #include "str2int.h"
 #include <assert.h>
-#include <stdio.h>
-#include <stdbool.h>
-#define int_MAX 2147483647
-#define int_MIN (-2147483647 - 1)
+#include "stdio.h"
 
-bool is_digit(char x) {
-    return (x >= '0' && x <= '9');
-}
+int str2int(const char* str)
+{
+	int znak = 1;
 
-int str2int(const char *str) {
-    int sign = 1;
-    int result = 0;
-    if (*str == '\0') {
-        assert(1 == 0);
-    }
+	int result = 0;
 
-    if (*str == '-') {
-        sign = -1;
-        str++;
-        if (*str == '\0') {
-            assert(1 == 0);
-        }
-  } else if (*str == '+') {
-      str++;
-  }
+	if (*str == '-')
+	{
+		znak = -1;
+		str++;
+	}
+	else if (*str == '+')
+	{
+		str++;
+	}
 
-    while (is_digit(*str)) {
-        int digit = *str - '0';
-        if (sign == 1 && (result > (int_MAX - digit) / 10)) {
-            assert(1 == 0);
-        } else if (sign == -1 && (result > (int_MIN - digit) / 10)) {
-            assert(1 == 0);
-        }
-        result = result * 10 + digit;
-        str++;
-    }
-    return sign * result;
+	int count = 0;
+	while (*str != '\0')
+	{
+		result = result * 10 + (*str - '0');
+		count += 1;
+		str++;
+		assert(znak == -1 && result == -2147483648 ||
+			   znak == -1 && result >= 0 && count <= 9 ||
+			   znak == 1 && result >= 0);
+	}
+	assert(count > 0);
+	return result * znak;
 }
