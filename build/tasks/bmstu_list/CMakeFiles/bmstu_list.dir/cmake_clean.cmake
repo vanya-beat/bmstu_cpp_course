cf13ce122176048449aaaa2699a8af6c831a96f2
howard <wantte@yandex.ru>
2025-06-04 21:52:33 +0300

    List updated. All tests passed. June 04 2025.

---
+++ build/tasks/bmstu_list/CMakeFiles/bmstu_list.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bmstu_list.dir/task_list/bmstu_list_test.cpp.o"
+  "CMakeFiles/bmstu_list.dir/task_list/bmstu_list_test.cpp.o.d"
+  "bmstu_list"
+  "bmstu_list.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bmstu_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/tasks/bmstu_list/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/howard/Desktop/bauman/bmstu_cpp_course/tasks/bmstu_list
+# Build directory: /home/howard/Desktop/bauman/bmstu_cpp_course/build/tasks/bmstu_list
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
