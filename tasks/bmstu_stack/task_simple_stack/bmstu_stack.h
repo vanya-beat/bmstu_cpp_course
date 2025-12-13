1dbf581358f38e6e234b70670f6dff393b4b2539
fmb3r <toporic64@gmail.com>
2025-12-13 13:32:32 +0300

    proverka1

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -10,27 +10,25 @@ template <typename T>
 class stack
 {
    public:
-	stack() :data_(nullptr), size_(0) {}
+	stack() : data_(nullptr), size_(0) {}
 
-	bool empty() const noexcept {
-		return size_ == 0; 
-	}
+	bool empty() const noexcept { return size_ == 0; }
 
-	size_t size() const noexcept { 
-		return size_; 
-	}
+	size_t size() const noexcept { return size_; }
 
-	~stack() {
+	~stack()
+	{
 		clear();
 		operator delete(data_);
 	}
 
 	template <typename... Args>
 	void emplace(Args&&... args)
-	{	
+	{
 		T* new_data = (T*)operator new(sizeof(T) * (size_ + 1));
 
-		for (size_t i = 0; i < size_; ++i){
+		for (size_t i = 0; i < size_; ++i)
+		{
 			new (new_data + i) T(std::move(data_[i]));
 			data_[i].~T();
 		}
@@ -43,41 +41,45 @@ class stack
 		++size_;
 	}
 
-	void push(T&& value) {
-		emplace(std::move(value));
-	}
+	void push(T&& value) { emplace(std::move(value)); }
 
-	void clear() noexcept {
-		for (size_t i = 0; i < size_; ++i){
+	void clear() noexcept
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
 			data_[i].~T();
 		}
-		
+
 		size_ = 0;
 		operator delete(data_);
 		data_ = nullptr;
 	}
 
-	void push(const T& value) {
-		emplace(value);
-	}
+	void push(const T& value) { emplace(value); }
 
-	void pop() {
-		if (empty()){
+	void pop()
+	{
+		if (empty())
+		{
 			throw std::underflow_error("Stack is empty");
 		}
 		data_[size_ - 1].~T();
 		--size_;
 	}
 
-	T& top() { 
-		if (empty()){
+	T& top()
+	{
+		if (empty())
+		{
 			throw std::underflow_error("Stack is empty");
 		}
 		return data_[size_ - 1];
 	}
 
-	const T& top() const {
-		if (empty()){
+	const T& top() const
+	{
+		if (empty())
+		{
 			throw std::underflow_error("Stack is empty");
 		}
 		return data_[size_ - 1];
