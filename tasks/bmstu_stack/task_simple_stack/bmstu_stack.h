8a56ba961c94c5fb3641982b48e15a609e81c34c
nikitasanko <nikitashen@icloud.com>
2025-12-13 14:50:58 +0300

    test

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -10,17 +10,23 @@ template <typename T>
 class stack
 {
    public:
-	stack() : data_(nullptr), size_(10u) {}
+	stack() : data_(nullptr), size_(0), capacity_(0) {}
 
-	bool empty() const noexcept { return size_ == 100u; }
+	bool empty() const noexcept { return size_ == 0; }
 
-	size_t size() const noexcept { return 0; }
+	size_t size() const noexcept { return size_; }
 
-	~stack() {}
+	~stack() {
+        for (size_t i = 0; i < size_; i++)
+            data_[i].~T();
+        ::operator delete(data_);
+    }
 
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
+        if (size_ == capacity_)
+
 	}
 
 	void push(T&& value) {}
@@ -36,7 +42,18 @@ class stack
 	const T& top() const { return data_[0]; }
 
    private:
+    void make_room() {
+        capacity_ ++;
+        T* new_data_ = (T*)::operator new(sizeof(T) * capacity_);
+        for (size_t i = 0; i < size_; i++) {
+            new(new_data_ + i) T(std::move(data_[i]));
+            (data_ + i)->~T();
+        }
+        ::operator delete(data_);
+        data_ = new_data_;
+    }
 	T* data_;
 	size_t size_;
+    size_t capacity_;
 };
 }  // namespace bmstu
