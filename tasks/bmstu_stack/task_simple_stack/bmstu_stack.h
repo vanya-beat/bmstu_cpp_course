f3f71c31881ecbf7bf0f73c1b3c31b33582527fc
NagovitcynK <emokid2007@icloud.com>
2025-12-20 13:16:01 +0300

    commit2

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <exception>
-#include <iostream>
 #include <utility>
 
 namespace bmstu
@@ -10,38 +9,35 @@ template <typename T>
 class stack
 {
    public:
-	stack() : data_(nullptr), size_(0), capacity_(0) {}
-
-	bool empty() const noexcept { return size_ == 0; }
-	size_t size() const noexcept { return size_; }
+	stack() : data_(nullptr), size_(0) {}
 
 	~stack()
 	{
 		clear();
-		::operator delete[](data_);
+		operator delete(data_);
 	}
+
+	bool empty() const noexcept { return size_ == 0; }
+	size_t size() const noexcept { return size_; }
 
 	template <typename... Args>
 	void emplace(Args&&... args)
 	{
-		ensure_capacity(size_ + 1);
-		new (data_ + size_) T(std::forward<Args>(args)...);
+		T* new_data = static_cast<T*>(::operator new((size_ + 1) * sizeof(T)));
+		for (size_t i = 0; i < size_; i++)
+		{
+			new (new_data + i) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (new_data + size_) T(std::forward<Args>(args)...);
+		operator delete(data_);
+		data_ = new_data;
 		size_++;
 	}
 
-	void push(const T& value)
-	{
-		ensure_capacity(size_ + 1);
-		new (data_ + size_) T(value);
-		size_++;
-	}
+	void push(const T& value) { emplace(value); }
 
-	void push(T&& value)
-	{
-		ensure_capacity(size_ + 1);
-		new (data_ + size_) T(std::move(value));
-		size_++;
-	}
+	void push(T&& value) { emplace(std::move(value)); }
 
 	void pop()
 	{
@@ -49,11 +45,12 @@ class stack
 			throw std::underflow_error("empty_error");
 		data_[size_ - 1].~T();
 		size_--;
+		// Можно не уменьшать массив — оставляем старую память для упрощения
 	}
 
 	void clear() noexcept
 	{
-		for (size_t i = 0; i < size_; ++i)
+		for (size_t i = 0; i < size_; i++)
 			data_[i].~T();
 		size_ = 0;
 	}
@@ -73,29 +70,7 @@ class stack
 	}
 
    private:
-	void ensure_capacity(size_t new_size)
-	{
-		if (new_size <= capacity_)
-			return;
-
-		size_t new_capacity = new_size;
-
-		T* new_data =
-			static_cast<T*>(::operator new[](new_capacity * sizeof(T)));
-
-		for (size_t i = 0; i < size_; i++)
-		{
-			new (new_data + i) T(std::move(data_[i]));
-			data_[i].~T();
-		}
-
-		::operator delete[](data_);
-		data_ = new_data;
-		capacity_ = new_capacity;
-	}
-
 	T* data_;
 	size_t size_;
-	size_t capacity_;
 };
 }  // namespace bmstu
