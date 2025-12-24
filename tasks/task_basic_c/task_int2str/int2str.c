efa37f2359a146f498d4988da3b775147130897c
vboxuser <aleksukx2007@gmail.com>
2025-12-25 01:58:05 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,9 +1,39 @@
 #include "int2str.h"
 #include <assert.h>
-#include "stdio.h"
+#include <stdio.h>
 
-char* int2str(int number)
-{
-	char* str = "0";
-	return str;
+char* int2str(int number) {
+    static char str[12]; // статический буфер
+    int i = 0;
+    int is_negative = 0;
+
+    if (number == 0) {
+        str[0] = '0';
+        str[1] = '\0';
+        return str;
+    }
+
+    if (number < 0) {
+        is_negative = 1;
+        number = -number;
+    }
+
+    while (number > 0) {
+        str[i++] = (number % 10) + '0';
+        number /= 10;
+    }
+
+    if (is_negative) {
+        str[i++] = '-';
+    }
+
+    str[i] = '\0';
+
+    for (int j = 0; j < i / 2; j++) {
+        char temp = str[j];
+        str[j] = str[i - j - 1];
+        str[i - j - 1] = temp;
+    }
+
+    return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,34 @@
 #include "str2int.h"
 #include <assert.h>
-#include "stdio.h"
+#include <stdio.h>
 
-int str2int(const char* str)
+int str2int(const char* str) 
 {
-	return 0;
+    assert(str != NULL);
+    
+    if (*str == '\0') {
+        printf("Пустая строка\n");
+        return 0;
+    }
+    
+    int result = 0;
+    int sign = 1;
+    
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+    
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    
+    if (*str != '\0') {
+        printf("Нет цифр\n");
+    }
+    
+    return result * sign;
 }
