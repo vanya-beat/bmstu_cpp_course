5b081ba8dc26896ba1360cdb03bb259d504676d9
kilkaai-coder <kilkaai07@gmail.com>
2025-12-26 21:31:28 +0300

    feat: complete stack implementation, all tests passed

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -1,42 +1,91 @@
 #pragma once
-
 #include <exception>
 #include <iostream>
 #include <utility>
+#include <stdexcept>
 
-namespace bmstu
-{
+namespace bmstu {
 template <typename T>
-class stack
-{
-   public:
-	stack() : data_(nullptr), size_(10u) {}
+class stack {
+public:
+    stack() : data_(nullptr), size_(0), capacity_(0) {}
 
-	bool empty() const noexcept { return size_ == 100u; }
+    bool empty() const noexcept {
+        return size_ == 0;
+    }
 
-	size_t size() const noexcept { return 0; }
+    size_t size() const noexcept {
+        return size_;
+    }
 
-	~stack() {}
+    ~stack() {
+        clear();
+        ::operator delete(data_);
+    }
 
-	template <typename... Args>
-	void emplace(Args&&... args)
-	{
-	}
+    template <typename... Args>
+    void emplace(Args&&... args) {
+        if (size_ == capacity_) {
+            reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+        }
+        new (data_ + size_) T(std::forward<Args>(args)...);
+        ++size_;
+    }
 
-	void push(T&& value) {}
+    void push(const T& value) {
+        emplace(value);
+    }
 
-	void clear() noexcept {}
+    void push(T&& value) {
+        emplace(std::move(value));
+    }
 
-	void push(const T& value) {}
+    void clear() noexcept {
+        while (size_ > 0) {
+            pop_quiet();
+        }
+    }
 
-	void pop() {}
+    void pop() {
+        if (empty()) {
+            throw std::underflow_error("Stack is empty!");
+        }
+        pop_quiet();
+    }
 
-	T& top() { return data_[0]; }
+    T& top() {
+        if (empty()) {
+            throw std::underflow_error("Stack is empty!");
+        }
+        return data_[size_ - 1];
+    }
 
-	const T& top() const { return data_[0]; }
+    const T& top() const {
+        if (empty()) {
+            throw std::underflow_error("Stack is empty!");
+        }
+        return data_[size_ - 1];
+    }
 
-   private:
-	T* data_;
-	size_t size_;
+private:
+    void reserve(size_t new_cap) {
+        T* new_data = static_cast<T*>(::operator new(new_cap * sizeof(T)));
+        for (size_t i = 0; i < size_; ++i) {
+            new (new_data + i) T(std::move(data_[i]));
+            (data_ + i)->~T();
+        }
+        ::operator delete(data_);
+        data_ = new_data;
+        capacity_ = new_cap;
+    }
+
+    void pop_quiet() noexcept {
+        --size_;
+        (data_ + size_)->~T();
+    }
+
+    T* data_;
+    size_t size_;
+    size_t capacity_;
 };
-}  // namespace bmstu
+}
